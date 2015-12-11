5638dd4c62ada95e698a25de8e10eb5b4a244589
lkho <llhmtc@gmail.com>
2015-12-11 23:02:55 +0800

    add Auth and DBMember models autoload url helper and auth model

---
+++ application/models/Auth_model.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * comp3421
+ * Created by LKHO.
+ * Date: 11/12/2015 19:08
+ */
+class Auth_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->model('DBMember');
+    }
+
+    /**
+     * get the current user, or null if not logged in
+     * @return DBMember|null
+     */
+    public function user()
+    {
+        return $this->session->userdata('user');
+    }
+
+    /**
+     * @return bool
+     */
+    public function loggedIn()
+    {
+        return isset($_SESSION['user']);
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     * @return bool
+     */
+    public function logIn($email, $password)
+    {
+        $this->load->database();
+        $user = $this->db->get_where('members', array('email' => $email))->row('DBMember');
+
+        if (isset($user)) {
+            // clear text password ;)
+            if (strcmp($password, $user->password) !== 0) {
+                return FALSE;
+            }
+            // remove the password
+            $user->password = NULL;
+            $user->is_admin = !empty($user->is_admin);
+            $this->session->set_userdata('user', $user);
+
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function logOut()
+    {
+        $this->session->unset_userdata('user');
+    }
+
+}
---
+++ application/models/DBMember.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * comp3421
+ * Created by LKHO.
+ * Date: 11/12/2015 21:09
+ */
+class DBMember extends CI_Model
+{
+    /**
+     * @var string
+     */
+    public $id;
+    /**
+     * @var string varchar(45) Dr, Prof, Mr, Ms, Mrs
+     */
+    public $title;
+    public $first_name;
+    /**
+     * @var string should be UPPERCASE
+     */
+    public $last_name;
+    public $address;
+    public $city;
+    public $country;
+    /**
+     * @var string student, participant, sponsor, vip, speaker
+     */
+    public $attendee_type;
+    public $department;
+    public $company;
+    /**
+     * @var string also used as login ID
+     */
+    public $email;
+    /**
+     * @var string this field is just for authentication and db insertion, it will always be null
+     * when retrieved from session
+     */
+    public $password;
+    /**
+     * @var string varchar(50)
+     */
+    public $phone_number;
+    /**
+     * @var string varchar(50)
+     */
+    public $fax_number;
+    /**
+     * @var string yyyy-mm-dd
+     */
+    public $registration_date;
+    /**
+     * @var string unpaid, cash, credit, eps, cheque
+     */
+    public $payment_status;
+    public $remarks;
+    /**
+     * @var bool
+     */
+    public $is_admin;
+
+    /**
+     * "$title $last_name, $first_name"<br>
+     * Dr NG, Vincent
+     * @return string
+     */
+    public function display_name()
+    {
+        return (isset($this->title) ? $this->title . ' ' : '') . $this->last_name . ', ' . $this->first_name;
+    }
+
+    /**
+     * change user's password
+     * @param string|null $old the old password, or null to bypass the check
+     * @param $new
+     * @return bool|int the affected rows, or false if fail due to wrong $old password or db error
+     */
+    public function change_password($old = NULL, $new)
+    {
+        $this->load->database();
+
+        if (isset($old)) {
+            $user = $this->db->select('password')->get_where('members', array('email' => $this->email))->row();
+            if (!isset($user)) {
+                return FALSE;
+            }
+            if (strcmp($old, $user->password) !== 0) {
+                return FALSE;
+            }
+        }
+
+        return $this->db->update('members', array('password' => $new), array('email' => $this->email));
+    }
+
+    /**
+     * alias for change_password(NULL, $new)
+     * @param $new
+     * @return bool|int the affected rows, or false if fail due to db error
+     */
+    public function reset_password($new)
+    {
+        return $this->change_password(NULL, $new);
+    }
+}
