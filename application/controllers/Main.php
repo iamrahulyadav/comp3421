ba7654cd33469c6b81865fc0fc7595fa51a8eeec
lkho <llhmtc@gmail.com>
2015-12-12 00:37:08 +0800

    -

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,9 +2,11 @@
 
 /**
  * comp3421
- * Created by admin.
+ * Created by LKHO.
  * Date: 9/12/2015 19:11
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Main extends CI_Controller
 {
 
