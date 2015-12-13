7ada3bcdb2e50dde569f78be8077adcecacc942f
lkho <llhmtc@gmail.com>
2015-12-13 23:48:33 +0800

    login form, register form

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,7 +10,14 @@ class Auth extends CI_Controller
 {
     public function login()
     {
-        $data = array();
+        $data = array(
+            'title'  => 'Log In',
+            'button' => 'Log In',
+            'fields' => array(
+                'email' => array('Email', 'email'),
+                'pw'    => array('Password', 'password')
+            )
+        );
 
         if ($this->input->method() == 'post') {
 
@@ -19,7 +26,10 @@ class Auth extends CI_Controller
 
             if (isset($email, $pw)) {
                 if ($this->auth->logIn($email, $pw)) {
-                    redirect('', 'location', 303);
+                    $x = json_encode(site_url());
+                    $this->output->append_output("<script>window.parent.location = $x;</script>");
+
+                    return;
                 } else {
                     $data['err'] = 'Wrong email or password';
                 }
@@ -30,12 +40,78 @@ class Auth extends CI_Controller
 
         }
 
-        $this->load->view('login.php', $data);
+        $this->load->view('simple_form', $data);
     }
 
     public function logout()
     {
         $this->auth->logOut();
         redirect('');
+    }
+
+    public function register()
+    {
+        check_access(TRUE, TRUE);
+
+        $data = array(
+            'title'  => 'Register',
+            'button' => 'Register',
+            'fields' => array(
+                'email'          => array('Email', 'email'),
+                'password'       => array('Password', 'password'),
+                'title'          => array('Title (Dr, Prof, Mr, Ms, Mrs)', 'text'),
+                'first_name'     => array('First Name', 'text'),
+                'last_name'      => array('Last Name', 'text'),
+                'address'        => array('Address', 'text'),
+                'city'           => array('City', 'text'),
+                'country'        => array('Country', 'text'),
+                'attendee_type'  => array('Attendee Type<br>(Student, Participant, Sponsor, VIP, Speaker)', 'text'),
+                'department'     => array('Department', 'text'),
+                'company'        => array('Company', 'text'),
+                'phone_number'   => array('Phone Number', 'text'),
+                'fax_number'     => array('Fax Number', 'text'),
+                'payment_status' => array('Payment Status<br>(Unpaid, Cash, Credit, EPS, Cheque)', 'text'),
+                'remarks'        => array('Remarks', 'text'),
+                'is_admin'       => array('Is administrator?', 'checkbox'),
+            )
+        );
+
+        if ($this->input->method() == 'post') {
+
+            $this->load->database();
+
+            $fields = $this->input->post();
+            foreach ($fields as &$v)
+                if ($v == "")
+                    $v = NULL;
+            $fields['is_admin'] = empty($fields['is_admin']);
+
+            if ($this->db
+                    ->set('registration_date', 'now()', FALSE)
+                    ->insert('members', $fields) === FALSE
+            ) {
+                $e = $this->db->error();
+                $e = json_encode($e['message']);
+                $this->output->append_output(
+                    "<script>
+                        alert('Database Error:\\n' + $e);
+                    </script>"
+                );
+            } else {
+                $x = json_encode(site_url());
+                $this->output->append_output(
+                    "<script>
+                        if(confirm('Register Success!\\nClick OK to enter a new record or cancel to go back to Home.'))
+                            window.parent.location.reload();
+                        else
+                            window.parent.location = $x;
+                    </script>"
+                );
+            }
+
+            return;
+        }
+
+        $this->load->view('simple_form', $data);
     }
 }
---
+++ application/views/simple_form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration and Attendance Managing Application</title>
+</head>
+<body>
+<h1><?php echo htmlspecialchars($title) ?></h1>
+
+<form method="post" target="async">
+    <table>
+        <?php
+
+        foreach ($fields as $key => $v) {
+            echo '<tr>';
+            echo "<td>{$v[0]}:</td>";
+            echo "<td><input name=\"$key\" type=\"{$v[1]}\"" . (!isset($first) ? ' required autofocus' : '') . ' /></td>';
+            echo '</tr>';
+            $first = TRUE;
+        }
+
+        ?>
+    </table>
+    <input type="submit" value="<?php echo $button ?>"/>
+</form>
+<iframe name="async" style="display: none"></iframe>
+<?php if (isset($err)) echo '<script>alert(' . json_encode($err) . ');</script>' ?>
+</body>
+</html>
