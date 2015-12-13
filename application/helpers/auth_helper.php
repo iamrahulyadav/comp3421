09cccd2908665f76df1b191e0ced87f30f508998
lkho <llhmtc@gmail.com>
2015-12-13 23:47:48 +0800

    add check_access helper

---
+++ application/helpers/auth_helper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * comp3421
+ * Created by LKHO.
+ * Date: 13/12/2015 23:27
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * CodeIgniter URL Helpers
+ *
+ * @package    CodeIgniter
+ * @subpackage Helpers
+ * @category   Helpers
+ * @author     LKHO
+ */
+
+// ------------------------------------------------------------------------
+
+function check_access($loggedIn, $admin)
+{
+    $loggedIn = $loggedIn || $admin;
+    if ($loggedIn) {
+        /** @var CI_Controller $ci */
+        $ci = get_instance();
+        if (!$ci->auth->loggedIn()) {
+            redirect('auth/login');
+        }
+        if ($admin && !$ci->auth->user()->is_admin) {
+            header('HTTP/1.1 401 Unauthorized');
+            echo
+            '<html>
+            <body>
+                <h2>You do not have permission to access this page</h2>
+                <a href="javascript:history.back()">Back</a>
+            </body>
+            </html>';
+            exit;
+        }
+    }
+
+}
