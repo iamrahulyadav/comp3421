5516075d7bf91b5eda968967bc2e776226fddd05
lkho <llhmtc@gmail.com>
2015-12-16 21:42:46 +0800

    update location fields, format code

--- application/views/conference_list.php
+++ application/views/conference_list.php
@@ -7,13 +7,12 @@
 <body>
 <?php echo $menu ?>
 <h1><?php echo htmlspecialchars($title) ?></h1>
-    <?php
+<?php
 
-if($this->auth->isLoggedIn()
-    &&$this->auth->user()->is_admin) {
-        echo "<button>create</button>";
-    }
-    echo "<table>";
+if ($this->auth->user()->is_admin) {
+    echo "<button>create</button>";
+}
+echo "<table>";
 echo '<tr>';
 echo '<th>No.</th>';
 echo '<th>Date</th>';
@@ -22,31 +21,29 @@ echo '<th>Topic</th>';
 echo '<th>Speaker</th>';
 echo '<th>Venue</th>';
 echo '<th>View Information</th>';
-if($this->auth->isLoggedIn()
-    &&$this->auth->user()->is_admin) {
+if ($this->auth->user()->is_admin) {
     echo '<th>Edit</th>';
     echo '<th>Delete</th>';
 }
 echo '</tr>';
-    for($i = 0;$i < count($data);$i++){
-        $startDate = date("Y-m-d",strtotime($data[$i]['start_time']));
-        $endDate = date("Y-m-d",strtotime($data[$i]['end_time']));
-        $startTime = date("H:i",strtotime($data[$i]['start_time']));
-        $endTime = date("H:i",strtotime($data[$i]['end_time']));
-        $no = $i+1;
-        echo '<tr>';
-        echo "<td>{$no}</td>";
-        echo "<td>{$startDate} - {$endDate}</td>";
-        echo "<td>{$startTime} - {$endTime}</td>";
-        echo "<td>{$data[$i]['topic']}</td>";
-        echo "<td>{$data[$i]['speaker']}</td>";
-        echo "<td>{$data[$i]['location']}</td>";
-        echo "<td><button>view information</button></td>";
-        if($this->auth->isLoggedIn()
-            &&$this->auth->user()->is_admin) {
-            echo "<td><button>edit</button></td>";
-            echo "<td><button>delete</button></td>";
-        }
+for ($i = 0; $i < count($data); $i++) {
+    $startDate = date("Y-m-d", strtotime($data[$i]['start_time']));
+    $endDate = date("Y-m-d", strtotime($data[$i]['end_time']));
+    $startTime = date("H:i", strtotime($data[$i]['start_time']));
+    $endTime = date("H:i", strtotime($data[$i]['end_time']));
+    $no = $i + 1;
+    echo '<tr>';
+    echo "<td>{$no}</td>";
+    echo "<td>{$startDate} - {$endDate}</td>";
+    echo "<td>{$startTime} - {$endTime}</td>";
+    echo "<td>{$data[$i]['topic']}</td>";
+    echo "<td>{$data[$i]['speaker']}</td>";
+    echo "<td>{$data[$i]['location_lat']}, {$data[$i]['location_long']}</td>";
+    echo "<td><button>view information</button></td>";
+    if ($this->auth->user()->is_admin) {
+        echo "<td><button>edit</button></td>";
+        echo "<td><button>delete</button></td>";
     }
+}
 echo "</table>";
-    ?>
+?>
