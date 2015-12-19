12692825f6a0f81204a7ae9eeae9c9e59f6d1167
lkho <llhmtc@gmail.com>
2015-12-19 18:52:01 +0800

    add create button

--- application/views/simple_list.php
+++ application/views/simple_list.php
@@ -8,6 +8,9 @@
 <body>
 <?php echo isset($menu) ? $menu : '' ?>
 <h1><?php echo htmlspecialchars($title) ?></h1>
+<?php if ($this->auth->user()->is_admin) {
+    echo "<a href=\"$create_url\"><button>Create</button></a>";
+} ?>
 <table border="1">
     <?php
     echo '<tr>';
