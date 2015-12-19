9739863780b743cdaf4ada7fef208a81f1ccdbbf
temp <makmingyipmatthew@hotmail.com>
2015-12-19 20:27:02 +0800

    no message

--- application/controllers/Rewards.php
+++ application/controllers/Rewards.php
@@ -8,7 +8,7 @@ require_once dirname(__FILE__) . '/CrudController.php';
  */
 class Rewards extends CrudController
 {
-    public $table = 'rewards';
+    public $table = 'reward';
     public $title = 'Rewards';
     public $view = array(
         'index'  => 'simple_list',
@@ -20,6 +20,25 @@ class Rewards extends CrudController
         'id'      => array('column' => 'No.'),
         'title'   => array('label' => 'Title', 'type' => 'text', 'column' => 'Title'),
         'content' => array('label' => 'Content', 'type' => 'textarea', 'column' => 'Content'),
-        'time'    => array('label' => 'Time', 'type' => 'datetime-local', 'title' => 'Time'),
+        'receive_id'   => array('label' => 'Receiver ID', 'type' => 'text', 'column' => 'Title'),
     );
+    public function index()
+    {
+        check_access(TRUE);
+
+        $data = array(
+            'title'      => $this->title,
+            'menu'       => $this->load->view('menu', NULL, TRUE),
+            'item_url'   => site_url(uri_string() . '/item/{id}'),
+            'create_url' => site_url(uri_string() . '/create'),
+            'edit_url'   => site_url(uri_string() . '/edit/{id}'),
+            'delete_url' => site_url(uri_string() . '/delete/{id}'),
+            'fields'     => $this->fields,
+        );
+
+        $r = $this->db->where('receiver_id', $this->auth->user()->id)->get($this->table);
+        $data['data'] = $r->result_array();
+
+        $this->load->view($this->view[__FUNCTION__], $data);
+    }
 }
