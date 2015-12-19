14574afe88a418f4cbcd14c852563fb2bd852451
lkho <llhmtc@gmail.com>
2015-12-20 02:43:22 +0800

    survey controller

--- application/controllers/Surveys.php
+++ application/controllers/Surveys.php
@@ -10,19 +10,77 @@ class Surveys extends CrudController
 {
     public $table = 'survey';
     public $title = 'Surveys';
+
     public $view = array(
-        'index'  => 'simple_list',
-        'detail'   => 'surveys_item',
-        'create' => 'simple_form',
-        'edit'   => 'simple_form',
+        'index'   => 'simple_list',
+        'detail'  => 'simple_list',
+        'create'  => 'simple_form',
+        'edit'    => 'simple_form',
     );
 
-    public $fields = array(
-        'id'               => array('column' => 'No.'),
-        'writer_id'        => array('label' => 'Writer ID', 'type' => 'text', 'column' => 'Writer ID'),
-        'topic'            => array('label' => 'Topic', 'type' => 'text', 'column' => 'Topic'),
-        'content'          => array('label' => 'Content', 'type' => 'textarea', 'column' => 'Content'),
-        'google_form_link' => array('label' => 'Link', 'type' => 'text', 'column' => 'Link'),
-        'deadline'         => array('label' => 'Deadline', 'type' => 'text', 'column' => 'Deadline'),
-    );
+    public function __construct()
+    {
+        parent::__construct();
+        $this->fields = array(
+            'id'           => array('column' => 'ID'),
+            'title'        => array('label' => 'Title', 'type' => 'text', 'column' => 'Title'),
+            'is_published' => array('label' => 'Published?', 'type' => 'checkbox', 'column' => 'Published?'),
+        );
+    }
+
+    public function detail($id)
+    {
+        check_access(TRUE, TRUE);
+
+        $data = array(
+            'title'      => 'Edit Survey Questions',
+            'menu'       => $this->load->view('menu', NULL, TRUE),
+            'create_url' => site_url(uri_string() . '/create'),
+            'edit_url'   => site_url(uri_string() . '/edit/{id}'),
+            'delete_url' => site_url(uri_string() . '/delete/{id}'),
+            'fields'     => array(
+                'order'       => array(
+                    'label'  => 'Display Order',
+                    'type'   => 'number',
+                    'attr'   => array('min' => 0, 'required' => ''),
+                    'column' => 'Display Order',
+                ),
+                'name'        => array(
+                    'label'  => 'Key (must be unique within question)',
+                    'type'   => 'text',
+                    'attr'   => array('required' => ''),
+                    'column' => 'Key',
+                ),
+                'description' => array(
+                    'label' => 'Description (html supported)',
+                    'type'  => 'textarea',
+                ),
+                'type'        => array(
+                    'label'  => 'Question type',
+                    'type'   => 'select',
+                    'values' => array(
+                        'text'           => 'Text',
+                        'textarea'       => 'Paragraph',
+                        'number'         => 'Number (integer only)',
+                        'datetime-local' => 'Date time',
+                        'checkbox'       => 'Yes No (checkbox)',
+                        'select'         => 'Choices (pull-down menu)',
+                        'radio'          => 'Choices (radio button)',
+                    ),
+                    'column' => 'Type',
+                ),
+                'options'     => array(
+                    'label' => 'Choices (if applicable, one line per item)',
+                    'type'  => 'textarea',
+                ),
+            ),
+        );
+
+        $r = $this->db->where('survey_id', $id)->order_by('order')->get($this->table . '_field');
+        $data['data'] = $r->result_array();
+        foreach ($data['data'] as &$v)
+            $v['id'] = $v['order'];
+
+        $this->load->view($this->view[__FUNCTION__], $data);
+    }
 }
