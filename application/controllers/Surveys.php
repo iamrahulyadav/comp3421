c3cd0e6d2e0894d880ad95ea5727372e43f3e8e4
lkho <llhmtc@gmail.com>
2015-12-20 03:59:11 +0800

    add view response column

--- application/controllers/Surveys.php
+++ application/controllers/Surveys.php
@@ -12,9 +12,10 @@ class Surveys extends CrudController
     public $title = 'Surveys';
 
     public $view = array(
-        'index'  => 'simple_list',
-        'create' => 'simple_form',
-        'edit'   => 'simple_form',
+        'index'    => 'simple_list',
+        'create'   => 'simple_form',
+        'edit'     => 'simple_form',
+        'response' => 'simple_list',
     );
 
     public function __construct()
@@ -24,6 +25,20 @@ class Surveys extends CrudController
             'id'           => array('column' => 'ID'),
             'title'        => array('label' => 'Title', 'type' => 'text', 'column' => 'Title'),
             'is_published' => array('label' => 'Published?', 'type' => 'checkbox', 'column' => 'Published?'),
+            'response'     => array('column' => 'View Response'),
         );
+    }
+
+    public function index()
+    {
+        $url = site_url(uri_string() . '/response') . '/';
+        $this->db->select(array('*', "concat('<a href=\"$url',id,'\"><button>View</button></a>') as response"));
+        parent::index();
+    }
+
+    public function response($id)
+    {
+        $this->table = 'survey_response';
+        
     }
 }
