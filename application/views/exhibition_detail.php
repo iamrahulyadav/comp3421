3a16328e59b28dd391e32e3c4eb6748c692d1f82
waynelwh <liuwangho2007@hotmail.com>
2015-12-20 03:13:27 +0800

    update

--- application/views/exhibition_detail.php
+++ application/views/exhibition_detail.php
@@ -3,6 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Registration and Attendance Managing Application</title>
+    <style>
+        table {
+            border       : 1px solid black;
+            table-layout : fixed;
+            width        : 400px;
+        }
+
+        td {
+            border   : 1px solid black;
+            overflow : hidden;
+            width    : 50px;
+        }
+    </style>
     <script type="text/javascript" src="http://www.google.com/jsapi"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzzmr04bM7hfgehfBvpTC7vFLEiugg6KE&callback=initMap"
             async defer></script>
@@ -13,7 +26,7 @@
         function drawMap() {
             var lat =<?php echo $data['venue_lat'] ?>;
             var lng =<?php echo $data['venue_lng'] ?>;
-            var myLatlng = new google.maps.LatLng(lat, lng,);
+            var myLatlng = new google.maps.LatLng(lat, lng);
             var myOptions = {
                 "zoom": 10,
                 "center": myLatlng,
@@ -28,19 +41,24 @@
 <h1><?php echo htmlspecialchars($title) ?></h1>
 <table>
     <tr>
-        <th><?php $data['company'] ?></th>
+        <td>Company: </td>
+        <td><?php $data['company'] ?></td>
     </tr>
     <tr>
+        <td>Start Time: </td>
         <td><?php $data['start_time'] ?></td>
     </tr>
     <tr>
+        <td>End Time: </td>
         <td><?php $data['end_time'] ?></td>
     </tr>
     <tr>
+        <td>Venue: </td>
         <td><?php $data['venue'] ?></td>
     </tr>
 </table>
 
 <div id="map" style="height: 450px;width: 300px">
 </div>
+</body>
 
