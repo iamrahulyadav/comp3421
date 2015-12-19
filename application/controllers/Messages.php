f9fbb53f28822a1723c798ecd4566a81d896bc5b
waynelwh <liuwangho2007@hotmail.com>
2015-12-19 20:15:50 +0800

    updated controller

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -11,9 +11,18 @@ class Messages extends CrudController
     public $table = 'messages';
     public $title = 'Messages Programs';
     public $view = array(
-        'index'  => 'messages_list',
+        'index'  => 'simple_list',
         'item'   => 'messages_item',
-        'create' => 'messages_create',
-        'edit'   => 'messages_edit'
+        'create' => 'simple_form',
+        'edit'   => 'simple_form',
+    );
+
+    public $fields = array(
+        'id'          => array('column' => 'No.'),
+        'sender_id'   => array('label' => 'Sender ID', 'type' => 'text', 'column' => 'Sender ID'),
+        'receiver_id' => array('label' => 'Receiver ID', 'type' => 'text', 'column' => 'Receiver ID'),
+        'title'       => array('label' => 'Title', 'type' => 'text', 'column' => 'Title'),
+        'content'     => array('label' => 'Content', 'type' => 'textarea', 'column' => 'Content'),
+        'time'        => array('label' => 'Time', 'type' => 'datetime', 'column' => 'Time'),
     );
 }
--- application/controllers/Surveys.php
+++ application/controllers/Surveys.php
@@ -8,12 +8,21 @@ require_once dirname(__FILE__) . '/CrudController.php';
  */
 class Surveys extends CrudController
 {
-    public $table = 'surveys';
+    public $table = 'survey';
     public $title = 'Surveys';
     public $view = array(
-        'index'  => 'surveys_list',
+        'index'  => 'simple_list',
         'item'   => 'surveys_item',
-        'create' => 'surveys_create',
-        'edit'   => 'surveys_edit'
+        'create' => 'simple_form',
+        'edit'   => 'simple_form',
+    );
+
+    public $fields = array(
+        'id'               => array('column' => 'No.'),
+        'writer_id'        => array('label' => 'Writer ID', 'type' => 'text', 'column' => 'Writer ID'),
+        'topic'            => array('label' => 'Topic', 'type' => 'text', 'column' => 'Topic'),
+        'content'          => array('label' => 'Content', 'type' => 'textarea', 'column' => 'Content'),
+        'google_form_link' => array('label' => 'Link', 'type' => 'text', 'column' => 'Link'),
+        'deadline'         => array('label' => 'Deadline', 'type' => 'text', 'column' => 'Deadline'),
     );
 }
