1501553bec811c9c8625a240c84ba14db16b45a3
lkho <llhmtc@gmail.com>
2015-12-20 03:44:16 +0800

    doSurvey

--- application/controllers/DoSurvey.php
+++ application/controllers/DoSurvey.php
@@ -37,13 +37,69 @@ class DoSurvey extends CrudController
             'fields'     => $this->processDynamicSource($this->fields, array(__FUNCTION__)),
         );
 
-        $r = $this->db->get($this->table);
+        $r = $this->db->get_where($this->table, array('is_published' => TRUE));
         $data['data'] = $r->result_array();
         $this->load->view($this->view[__FUNCTION__], $data);
     }
 
     public function create($survey_id = 0)
     {
+        check_access(TRUE);
 
+        $r = $this->db->select('title')->get_where('survey', array('id' => $survey_id));
+        $r = $r->row_array();
+        $title = reset($r);
+
+        $fields = array();
+
+        $r = $this->db->order_by('order')->get_where('survey_field', array('survey_id' => $survey_id));
+        foreach ($r->result_array() as $row) {
+            $fields[$row['id']] = array(
+                'label' => $row['description'],
+                'type'  => $row['type'],
+                'attr'  => empty($row['is_required']) ? array() : array('required' => ''),
+            );
+            if (!empty($row['options'])) {
+                $options = explode(PHP_EOL, $row['options']);
+                $fields[$row['id']]['values'] = array_combine($options, $options);
+            }
+        }
+
+        $data = array(
+            'title'  => 'Survey: ' . $title,
+            'menu'   => $this->load->view('menu', NULL, TRUE),
+            'button' => 'Submit',
+            'form'   => array(
+                'action' => site_url(uri_string()),
+                'method' => 'post',
+            ),
+            'fields' => $fields,
+        );
+
+        $this->load->view($this->view[__FUNCTION__], $data);
+    }
+
+    public function create_post($survey_id = 0)
+    {
+        check_access(TRUE);
+
+        $set = array(
+            'survey_id'     => $survey_id,
+            'writer_id'     => $this->auth->user()->id,
+            'response_json' => json_encode($_POST),
+        );
+
+        if ($this->db->insert($this->table, $set) !== FALSE) {
+            $list = json_encode(site_url(dirname(uri_string())));
+
+            $this->output->append_output(
+                "<script>
+                    alert('Thank you for your participation!');
+                    window.parent.location = $list;
+                </script>"
+            );
+        } else {
+            $this->dbError();
+        }
     }
 }
