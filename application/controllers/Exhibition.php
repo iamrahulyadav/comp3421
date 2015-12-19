2b09fb52198e1dca9245789d12d617f1b64b37b4
temp <makmingyipmatthew@hotmail.com>
2015-12-19 17:37:52 +0800

    no message

--- application/controllers/Exhibition.php
+++ application/controllers/Exhibition.php
@@ -11,21 +11,9 @@ class Exhibition extends CrudController
     public $table = 'exhibition';
     public $title = 'Exhibitions';
     public $view = array(
-        'index'  => 'simple_list',
+        'index'  => 'exhibition_list',
         'item'   => 'exhibition_item',
-        'create' => 'simple_form',
-        'edit'   => 'simple_form'
+        'create' => 'exhibition_create',
+        'edit'   => 'exhibition_edit'
     );
-
-    public $list = array(
-    	'column1'=> array('title'=>'No.','db_column_name'=>'id'),
-    	'column2'=> array('title'=>'Start Date','db_column_name'=>'start_date'),
-    	'column3'=> array('title'=>'End Date','db_column_name'=>'end_date'),
-    	'column4'=> array('title'=>'Start Time','db_column_name'=>'start_time'),
-    	'column5'=> array('title'=>'End Time','db_column_name'=>'end_time'),
-    	'column6'=> array('title'=>'Name','db_column_name'=>'name'),
-    	'column7'=> array('title'=>'Venue','db_column_name'=>'venue'),
-    	'column8'=> array('title'=>'Company','db_column_name'=>'company'),
-    	'column9'=> array('title'=>'View Infomation','db_column_name'=>'info'),
-    	);
 }
