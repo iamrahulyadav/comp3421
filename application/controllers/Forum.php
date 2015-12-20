7c2d007f87cb705b05deb80b8397e077acfd41fd
KingHoRonaldLono <iamwongkingho@gmail.com>
2015-12-20 10:35:40 +0800

    no message

--- application/controllers/Forum.php
+++ application/controllers/Forum.php
@@ -19,6 +19,49 @@ class Forum extends CrudController
     public $fields = array(
         'id' => array('column' => 'No.'),
         'topic' => array('label'=>'Topic','type'=>'textarea','column'=>'Topic'),
-        'time' => array('label' => 'Time', 'type' => 'datetime')
+        'time' => array('label' => 'Time', 'type' => 'datetime','column'=>'Time')
     );
+
+    public $item_table = 'forum_article';
+    public $item_fields = array(
+        'id' => array('label' => 'id'),
+        'title' => array('label'=>'Title','type'=>'textarea','column'=>'Title'),
+        'content' => array('label'=>'Content','type'=>'textarea','column'=>'Content'),
+        'writer' => array('label'=>'Writer','type'=>'textarea','column'=>'writer_id'),
+        'time' => array('label' => 'Time', 'type' => 'datetime','column'=>'Time'),
+        'reply' => array('label'=>'reply','type'=>'textarea','column'=>'reply_to'),
+    );
+
+    public function detail($id)
+    {
+        check_access(TRUE);
+
+        $data = array(
+            'title'      => $this->title . ' Details',
+            'menu'       => $this->load->view('menu', NULL, TRUE),
+            'edit_url'   => site_url(dirname(uri_string()) . '/edit/{id}'),
+            'delete_url' => site_url(uri_string()),
+            'fields'     => $this->processDynamicSource($this->fields, array(__FUNCTION__, $id)),
+        );
+
+        $r = $this->db->where('id', $id)->get($this->table);
+        $r = $r->result_array();
+        $r = reset($r);
+        $data['data'] = $this->processItemSource($r, __FUNCTION__);
+        $this->load->view($this->view[__FUNCTION__], $data);
+
+        $item_data = array(
+            'title'      => $this->title . ' Details',
+//            'menu'       => $this->load->view('menu', NULL, TRUE),
+            'edit_url'   => site_url(dirname(uri_string()) . '/edit/{id}'),
+            'delete_url' => site_url(uri_string()),
+            'fields'     => $this->processDynamicSource($this->fields, array(__FUNCTION__, $id)),
+        );
+
+        $r = $this->db->where('forum_id', $id)->get($this->item_table);
+        $r = $r->result_array();
+        $r = reset($r);
+        $item_data['data'] = $this->processItemSource($r, __FUNCTION__);
+//        $this->load->view($this->view[__FUNCTION__], $item_data);
+    }
 }
---
+++ application/views/forum_item.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        table {
+            border       : 1px solid black;
+            table-layout : fixed;
+            width        : 400px;
+        }
+
+        table.item{
+            border       : 1px solid black;
+            table-layout : fixed;
+            width        : 100%;
+        }
+
+        td {
+            border   : 1px solid black;
+            overflow : hidden;
+            width    : 50px;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <title>Registration and Attendance Managing Application</title>
+    <script src="<?php echo base_url('jquery/jquery.js') ?>"></script>
+</head>
+<body>
+<?php echo $menu ?>
+<h1><?php echo htmlspecialchars($title) ?></h1>
+
+
+<table>
+    <tr>
+        <td><?php echo $data['id']; ?></td>
+        <td><?php echo $data['topic']; ?></td>
+        <td><?php echo $data['time']; ?></td>
+    </tr>
+</table>
+<br>
+<table class="item">
+    <tr>
+        <td>No.</td>
+        <td><?php echo $item_data['id']; ?></td>
+    </tr>
+    <tr>
+        <td>Topic</td>
+        <td><?php echo $item_data['title']; ?></td>
+    </tr>
+    <tr>
+        <td>Time</td>
+        <td><?php echo $item_data['content']; ?></td>
+    </tr>
+</table>
+
+<?php
+echo '<a href="' . site_url("forum") . '"><button>Back</button></a >'
+?>
+</body>
+</html>
