ed8018a5fcb724600cf3f362c7e3dd2d194b759b
waynelwh <liuwangho2007@hotmail.com>
2015-12-20 16:07:18 +0800

    test

--- application/views/menu.php
+++ application/views/menu.php
@@ -6,38 +6,38 @@
  */
 ?>
 <link rel="stylesheet" type="text/css" href="../css/menu.css"/>
-<h1>Registration and Attendance Managing Application</h1>
-
-<p>Welcome<?php
-    $urls = array();
-    if ($this->auth->isLoggedIn()) {
-        $urls['View Conference Sessions'] = 'sessions';
-        $urls['View Exhibitions'] = 'exhibition';
-        $urls['View Forums'] = 'forum';
-        $urls['View Announcements'] = 'announcements';
-        $urls['View Rewards'] = 'rewards';
-        $urls['View Messages'] = 'messages';
-        $urls['Do Survey'] = 'doSurvey';
-        if ($this->auth->user()->is_admin) {
-            $urls['Edit Surveys'] = 'surveys';
-            $urls['Register Member'] = 'auth/register';
-            $urls['View Members'] = 'members';
-        }
-    } else {
+<p>Registration and Attendance Managing Application</p>
+<?php
+$urls = array();
+if ($this->auth->isLoggedIn()) {
+    $urls['View Conference Sessions'] = 'sessions';
+    $urls['View Exhibitions'] = 'exhibition';
+    $urls['View Forums'] = 'forum';
+    $urls['View Announcements'] = 'announcements';
+    $urls['View Rewards'] = 'rewards';
+    $urls['View Messages'] = 'messages';
+    $urls['Do Survey'] = 'doSurvey';
+    if ($this->auth->user()->is_admin) {
+        $urls['Edit Surveys'] = 'surveys';
+        $urls['Register Member'] = 'auth/register';
+        $urls['View Members'] = 'members';
     }
-
-    if ($this->auth->isLoggedIn()) {
-        echo ' ' . $this->auth->user()->display_name() . '!';
-        if ($this->auth->user()->is_admin)
-            echo ' [admin]';
-        echo ' (<a href="' . site_url('auth/logout') . '">Log out</a>)';
-    } else {
-        echo '! (<a href="' . site_url('auth/login') . '">Log in</a>)';
-    } ?></p>
+}
+?>
 <table id="menuBar">
     <tr>
-    <?php foreach ($urls as $title => $url) {
-        echo '<td><a href="' . site_url($url) . '" title="' . $title . '">' . $title . '</a ></td > ';
-    } ?>
+        <?php
+        foreach ($urls as $title => $url) {
+            echo '<td><a href="' . site_url($url) . '" title="' . $title . '">' . $title . '</a ></td > ';
+        }
+        if ($this->auth->isLoggedIn()) {
+            echo '<td>' . $this->auth->user()->display_name() . '!</td>';
+            if ($this->auth->user()->is_admin)
+                echo ' [admin]';
+            echo ' <td><a href="' . site_url('auth/logout') . '">Logout</a></td>';
+        } else {
+            echo '<td><a href="' . site_url('auth/login') . '">Login</a></td>';
+        }
+        ?>
     </tr>
 </table>
